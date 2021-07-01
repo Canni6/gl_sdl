336ad5b7fbb69641cf60d0a4bc3ac640f71fafd4
Lachlan Blennerhassett <lachlanblennerhassett@gmail.com>
2021-07-01 18:16:15 +1000

    Multiple vertex attributes - pg 49

--- opengl_sdl/src/sdl.cpp
+++ opengl_sdl/src/sdl.cpp
@@ -11,20 +11,58 @@
 #define SDL_MAIN_HANDLED 
 #include <SDL2/SDL.h>
 
-// Compile a vertex shader
+//// Compile a vertex shader
+//const char* vertexShaderSource = "#version 330 core\n"
+//"layout (location = 0) in vec3 aPos;\n"
+//"out vec4 vertexColor;\n" // specify a color output to fragment shader"
+//"void main()\n"
+//"{\n"
+//"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+//"vertexColor = vec4(0.5, 0, 0.5, 1.0);\n"
+//"}\0";
+
+// Compile a vertex shader - this time with another attribute
 const char* vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 0) in vec3 aPos;\n" // position has attribute position 0
+"layout (location = 1) in vec3 aColor;\n" // color has attribute position 1
+
+"out vec3 ourColor;\n" // output a color to fragment shader"
 "void main()\n"
 "{\n"
-" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"gl_Position = vec4(aPos, 1.0);\n"
+"ourColor = aColor;\n"
 "}\0";
+
+
+//// Send data from vertex shader to fragment shader
+//
+//// Compile a fragment shader
+//const char* fragmentShaderSource = "#version 330 core\n"
+//"out vec4 FragColor;\n"
+//"in vec4 vertexColor;\n"
+//"void main()\n"
+//"{\n"
+//"FragColor = vertexColor;\n"
+//"}\0";
+
+// Send data from vertex shader to fragment shader
+
+//// Compile a fragment shader
+//const char* fragmentShaderSource = "#version 330 core\n"
+//"out vec4 FragColor;\n"
+//"in vec4 vertexColor;\n"
+//"void main()\n"
+//"{\n"
+//"FragColor = vertexColor;\n"
+//"}\0";
 
 // Compile a fragment shader
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-" FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"FragColor = vec4(ourColor, 1.0);\n"
 "}\0";
 
 
@@ -94,16 +132,27 @@ int main(int argc, char** argv) {
     // When drawing the above triangles, redundant/repeated vertices occur
     // Element buffer objects take care of this ^
     // Instead we can do this:
+    //float vertices[] = {
+    //    0.5f, 0.5f, 0.0f, // top right
+    //    0.5f, -0.5f, 0.0f, // bottom right
+    //    -0.5f, -0.5f, 0.0f, // bottom left
+    //    -0.5f, 0.5f, 0.0f // top left
+    //};
+
+    // Let's instead do position and colors
+    // R G B to each of the corners of the triangle
     float vertices[] = {
-        0.5f, 0.5f, 0.0f, // top right
-        0.5f, -0.5f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f, // bottom left
-        -0.5f, 0.5f, 0.0f // top left
+        // positions            // colors
+        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f, // bottom right
+       -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f, // bottom left
+        0.0f, 0.5f, 0.0f,   0.0f, 0.0f, 1.0f  // top
     };
-    unsigned int indices[] = { // note that we start from 0!
-    0, 1, 3, // first triangle
-    1, 2, 3 // second triangle
-    };
+
+
+    //unsigned int indices[] = { // note that we start from 0!
+    //0, 1, 3 // first triangle
+    ////1, 2, 3 // second triangle
+    //};
 
     // create vertex shader object
     unsigned int vertexShader;
@@ -155,31 +204,46 @@ int main(int argc, char** argv) {
     // Copy vertex data into buffer memory
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // Instruct OpenGL how to interpret vertex data
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+    //// Instruct OpenGL how to interpret vertex data
+    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+    //    (void*)0);
+    //glEnableVertexAttribArray(0);
+
+    // Position attribute
+    // Instruct OpenGL how to interpret vertex data - setting stride to 6 this time
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
         (void*)0);
     glEnableVertexAttribArray(0);
 
-    // Generate a Vertex Array Object (VAO) to store info on vertex attribute configs and buffer objects
-    unsigned int VAO;
-    glGenVertexArrays(1, &VAO);
-    // 1. bind Vertex Array Object
-    glBindVertexArray(VAO);
-    // 2. copy our vertices array in a buffer for OpenGL to use
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-    // 3. then set our vertex attributes pointers
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
-    (void*)0);
-    glEnableVertexAttribArray(0);
+    // Color attribute - setting location to 1
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float),
+        (void*)(3 * sizeof(float))); // color attrib starts after position data so offset is 3 floats (12 bytes) - see pg. 49
+    glEnableVertexAttribArray(1);
+    //// Generate a Vertex Array Object (VAO) to store info on vertex attribute configs and buffer objects
+    //unsigned int VAO;
+    //glGenVertexArrays(1, &VAO);
+    //// 1. bind Vertex Array Object
+    //glBindVertexArray(VAO);
+    //// 2. copy our vertices array in a buffer for OpenGL to use
+    //glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    //glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    //// 3. then set our vertex attributes pointers
+    //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float),
+    //(void*)0);
+    
 
-    // Create element buffer object
-    unsigned int EBO;
-    glGenBuffers(1, &EBO);
-    // Bind EBO and copy indices into the buffer
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-        GL_STATIC_DRAW);
+    //// Create element buffer object
+    //unsigned int EBO;
+    //glGenBuffers(1, &EBO);
+    //// Bind EBO and copy indices into the buffer
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+    //    GL_STATIC_DRAW);
+
+    // Either fill (GL_FILL) or wireframe mode (GL_LINE)
+    /*glPolygonMode(GL_FRONT_AND_BACK,
+        GL_LINE);*/
+
 
     // main render loop
     while (running) {
@@ -198,13 +262,11 @@ int main(int argc, char** argv) {
             }
         }
 
-        glClearColor(0, 0, 0, 1);
+        //glClearColor(0, 0, 0, 1);
         // The triangle we've all been waiting for
-        glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
-        //glDrawArrays(GL_TRIANGLES, 0, 3);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-        glBindVertexArray(0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        //glBindVertexArray(0);
 
 
         SDL_GL_SwapWindow(window);
